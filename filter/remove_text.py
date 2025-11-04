0ef0fa5927a5710dc57ddc3edbca745c882aa97c
khoile903 <khoile903@gmail.com>
2025-11-05 03:22:29 +0700

    add: log check 2

--- filter/remove_text.py
+++ filter/remove_text.py
@@ -41,6 +41,8 @@ def remove_text(texts, texts_remove=REMOVE_TEXTS.text):
                 end_word = start_word + len(rem_word) + 1
                 normalized_text = normalized_text[:start_word] + normalized_text[end_word:]
                 text = text[:start_word] + text[end_word:]
+                print(f"    - New normalized text: {normalized_text}")
+                print(f"    - New original text: {text}")
         # Chỉ thêm vào kết quả nếu không rỗng và có độ dài > 1
         if text and len(text) > 1:
             result.append(text)
